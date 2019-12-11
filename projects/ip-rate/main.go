af214d4e631328e0faa78bd443ad372d2fdd90a0
liangjisheng <1294851990@qq.com>
2019-12-11 20:58:52 +0800

    request rate limit

---
+++ projects/ip-rate/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+var limiter = NewIPRateLimiter(1, 5)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", okHandler)
+
+	if err := http.ListenAndServe("127.0.0.1:8080", limitMiddleware(mux)); err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
+	}
+}
+
+func limitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		limiter := limiter.GetLimiter(r.RemoteAddr)
+		if !limiter.Allow() {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func okHandler(w http.ResponseWriter, r *http.Request) {
+	// Some very expensive database call
+	w.Write([]byte("alles gut"))
+}
