d2bb60849ce24bf87e8544397df7d212cb795ebb
liangjisheng <1294851990@qq.com>
2020-02-23 21:15:32 +0800

    distributed lock

---
+++ projects/go-zk-lock/README.md
@@ -0,0 +1,5 @@
+# go-zk-lock
+
+a distributed locker based on zookeeper and implemented in golang.
+
+[github](https://github.com/nladuo/go-zk-lock)
---
+++ projects/go-zk-lock/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	DLocker "github.com/nladuo/go-zk-lock"
+)
+
+var (
+	hosts         = []string{"117.51.148.112:2181"} // the zookeeper hosts
+	basePath      = "/locker"                       //the application znode path
+	lockerTimeout = 5 * time.Second                 // the maximum time for a locker waiting
+	zkTimeOut     = 20 * time.Second                // the zk connection timeout
+)
+
+func main() {
+	end := make(chan byte)
+	err := DLocker.EstablishZkConn(hosts, zkTimeOut)
+	defer DLocker.CloseZkConn()
+	if err != nil {
+		panic(err)
+	}
+	// concurrent test lock and unlock
+	for i := 0; i < 100; i++ {
+		go run(i)
+	}
+	<-end
+}
+
+func run(i int) {
+	locker := DLocker.NewLocker(basePath, lockerTimeout)
+	for {
+		locker.Lock() // like mutex.Lock()
+		fmt.Println("gorountine", i, ": get lock")
+		// do something of which time not excceed lockerTimeout
+		fmt.Println("gorountine", i, ": unlock")
+		if !locker.Unlock() { // like mutex.Unlock(), return false when zookeeper connection error or locker timeout
+			log.Println("gorountine", i, ": unlock failed")
+		}
+	}
+}
