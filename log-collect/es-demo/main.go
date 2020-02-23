d1f8350407208aa7f5594903bc8160f2093a1259
liangjisheng <1294851990@qq.com>
2020-02-23 21:14:59 +0800

    elastic demo

--- log-collect/es-demo/main.go
+++ log-collect/es-demo/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/olivere/elastic"
+	"github.com/olivere/elastic/v7"
 )
+
+var client *elastic.Client
+var host = "http://117.51.148.112:9200/"
 
 // Tweet ...
 type Tweet struct {
@@ -13,21 +18,30 @@ type Tweet struct {
 	Message string
 }
 
-func main() {
-	client, err := elastic.NewClient(elastic.SetSniff(false), elastic.SetURL("http://117.51.148.112:9200/"))
+func init() {
+	errorlog := log.New(os.Stdout, "APP", log.LstdFlags)
+	var err error
+	client, err = elastic.NewClient(elastic.SetSniff(false), elastic.SetErrorLog(errorlog), elastic.SetURL(host))
 	if err != nil {
-		fmt.Println("connect es error", err)
-		return
+		fmt.Println("new client err: ", err)
+		os.Exit(-1)
 	}
-	fmt.Println("conn es succ")
+	info, code, err := client.Ping(host).Do(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
+
+	esversion, err := client.ElasticsearchVersion(host)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Elasticsearch version %s\n", esversion)
+}
+
+func main() {
 	tweet := Tweet{User: "olivere name", Message: "Take Five"}
-	_, err = client.Index().
-		Index("jw").
-		// Type("tweet").
-		Type("employee").
-		Id("1").
-		BodyJson(tweet).
-		Do(context.TODO())
+	_, err := client.Index().Index("jw").Type("employee").Id("4").BodyJson(tweet).Do(context.TODO())
 	if err != nil {
 		panic(err)
 	}
