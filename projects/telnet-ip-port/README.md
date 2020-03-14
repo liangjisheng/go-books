694a78965a32633916547e8d9ceb49b3be44ffd4
liangjisheng <1294851990@qq.com>
2020-03-14 19:28:19 +0800

    telnet ip port

---
+++ projects/telnet-ip-port/README.md
@@ -0,0 +1,3 @@
+# README
+
+go实现类似telnet端口通不通,如果能与目标ip和port建立tcp连接,则认为是通的,反之则不通
---
+++ projects/telnet-ip-port/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+func tcpGather(ip string, ports []string) map[string]string {
+	results := make(map[string]string)
+	for _, port := range ports {
+		address := net.JoinHostPort(ip, port)
+		// 3 秒超时
+		conn, err := net.DialTimeout("tcp", address, 3*time.Second)
+		if err != nil {
+			results[port] = "failed"
+			// todo log handler
+		} else {
+			if conn != nil {
+				results[port] = "success"
+				_ = conn.Close()
+			} else {
+				results[port] = "failed"
+			}
+		}
+	}
+	return results
+}
+
+func main() {
+	fmt.Printf("%v\n", tcpGather("127.0.0.1", []string{"8080"}))
+}
