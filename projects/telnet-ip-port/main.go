68a2c3ff2f364b4105e406bd1dfd96d620ee4623
liangjisheng <1294851990@qq.com>
2020-03-24 22:54:46 +0800

    go telnet

--- projects/telnet-ip-port/main.go
+++ projects/telnet-ip-port/main.go
@@ -6,6 +6,21 @@ import (
 	"time"
 )
 
+// DetecdIPAndPort 检测本机是否可以和给定的ip端口建立TCP连接
+func DetecdIPAndPort(ip, port string) bool {
+	address := net.JoinHostPort(ip, port)
+	// 3 秒超时
+	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
+	if err != nil {
+		return false
+	}
+	if conn != nil {
+		_ = conn.Close()
+		return true
+	}
+	return false
+}
+
 func tcpGather(ip string, ports []string) map[string]string {
 	results := make(map[string]string)
 	for _, port := range ports {
